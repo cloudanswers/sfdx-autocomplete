65b42db54ac6b6828f3f8646e66a474f839e9619
James Sullivan <jsullivan@cambridgecloudpartners.com>
2017-07-06 11:00:07 -0400

    remove extra newlines

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 set -e
 git clone git@github.com:cloudanswers/sfdx-autocomplete.git ~/.sfdx-autocomplete
-echo "\n\n[ -s ~/.sfdx-autocomplete/sfdx-autocomplete.sh ] && . ~/.sfdx-autocomplete/sfdx-autocomplete.sh" >> ~/.bashrc
+echo "[ -s ~/.sfdx-autocomplete/sfdx-autocomplete.sh ] && . ~/.sfdx-autocomplete/sfdx-autocomplete.sh" >> ~/.bashrc
