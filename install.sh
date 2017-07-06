279d0c58f219892f0190e68cf539e94e3fa29c45
James Sullivan <jsullivan@cambridgecloudpartners.com>
2017-07-06 10:36:44 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+git clone git@github.com:cloudanswers/sfdx-autocomplete.git ~/.sfdx-autocomplete
+echo -e "\n\n[ -s ~/.sfdx-autocomplete/sfdx-autocomplete.sh ] && . ~/.sfdx-autocomplete/sfdx-autocomplete.sh" >> ~/.bashrc
