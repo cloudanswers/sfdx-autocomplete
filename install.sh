2dd14c0b56b5753c0935bdb53d401a6c33f2941a
James Sullivan <jsullivan@cambridgecloudpartners.com>
2017-07-06 10:58:20 -0400

    cleaner errors

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
+set -e
 git clone git@github.com:cloudanswers/sfdx-autocomplete.git ~/.sfdx-autocomplete
-echo -e "\n\n[ -s ~/.sfdx-autocomplete/sfdx-autocomplete.sh ] && . ~/.sfdx-autocomplete/sfdx-autocomplete.sh" >> ~/.bashrc
+echo "\n\n[ -s ~/.sfdx-autocomplete/sfdx-autocomplete.sh ] && . ~/.sfdx-autocomplete/sfdx-autocomplete.sh" >> ~/.bashrc
